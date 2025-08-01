abf4b82b3c9822b04545ffb336c6e8257ff9c7f1
caio reis <caioreis00p@gmail.com>
2025-08-01 18:21:11 -0300

    up

--- index.js
+++ index.js
@@ -61,3 +61,15 @@
     }
 
     digita();
+
+
+    /* <div class="txtsobre">
+                <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#a48edb"><path d="M320-242 80-482l242-242 43 43-199 199 197 197-43 43Zm318 2-43-43 199-199-197-197 43-43 240 240-242 242Z"/></svg>
+             
+                <p><i class="fa-brands fa-html5"></i>  Html 5</p>
+                <p><i class="fa-brands fa-css3"></i>  Css 3</p>
+                <p><i class="fa-brands fa-react"></i>  React</p>
+                <p><i class="fa-brands fa-square-js"></i>  JavaScrpit</p>
+                <p><i class="fa-brands fa-wordpress-simple"></i>  wordpress</p> 
+                <p><i class="fa-brands fa-typescript"></i>  TypeScript</p> 
+                <p><i class="fa-brands fa-figma"></i>  Figma</p>*/
